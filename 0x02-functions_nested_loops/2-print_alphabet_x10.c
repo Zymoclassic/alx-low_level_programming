b83e822920b0187b8f2da15cf087fd1ee66f97b2
Awopeju Kolawole Moses <awopejumoses@gmail.com>
2023-03-21 19:07:29 -0700

    Print Alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10- prints all the alphabets ten times
+ */
+void print_alphabet_x10(void)
+{
+char a_z;
+int i;
+i = 0;
+while (i < 10)
+{
+a_z = 'a';
+while (a_z <= 'z')
+{
+_putchar(a_z);
+a_z++;
+}
+_putchar('\n');
+i++;
+}
+}
