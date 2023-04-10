36e9fda6f682fe6e6e373d990a0ab0c74560d99f
Awopeju Kolawole Moses <awopejumoses@gmail.com>
2023-04-10 22:34:56 +0000

    advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - minimum number of coin to make change
+ *
+ * @argc: number of command line argument
+ *
+ * @argv: array of size argc
+ *
+ * Return: 0(success)
+ */
+int main(int argc, char **argv)
+{
+	int coins = 0, sum = 0;
+
+	x
+		if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	sum = atoi(argv[1]);
+
+	if (sum < 0)
+	{
+		printf("0\n");
+
+		return (0);
+	}
+	while (sum / 25)
+	{
+		sum = sum - 25;
+		coins++;
+	}
+	while (sum / 10)
+	{
+		sum = sum - 10;
+		coins++;
+	}
+	while (sum / 5)
+	{
+		sum = sum - 5;
+		coins++;
+	}
+	while (sum / 2)
+	{
+		sum = sum - 2;
+		coins++;
+	}
+	coins = coins + sum
+		printf("%d\n", coins);
+		return (0);
+}
