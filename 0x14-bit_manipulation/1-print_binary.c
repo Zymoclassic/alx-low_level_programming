898d0628c90880b146d9e4cabc5f2f8314041a42
zymoclassic <awopejumoses@gmail.com>
2023-05-05 07:21:00 -0700

    Manipulations

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
+ * print_binary - Prints binary
  *
- * Return: no return.
+ * @n: Unsigned int
+ *
+ * Return: Converted number
  */
+
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+
+	if (n > 1)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+	print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
 	}
 	else
 	{
 		_putchar('0');
 	}
+
+
 }
